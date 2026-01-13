a888e333603ec95562114d070417c4325b36bc55
Nileshsri2022 <2022021140@mmmut.ac.in>
2026-01-13 16:09:18 +0530

    fix: upgrade React to 18.3.1 and fix not-found.tsx component

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,17 +1,14 @@
-"use client";
 import Image from 'next/image'
 import React from 'react'
 import ErrorImage from "../public/404.svg"
 import Link from 'next/link';
 
-const error = () => {
+export default function NotFound() {
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center bg-base-100'>
-        <Image src={ErrorImage} alt="404 Not Found"/>
-        <p className='font-bold text-3xl mb-3'>Page not found!</p>
-        <Link href={"/"} className='font-semibold underline underline-offset-2 hover:text-primary-500 transition-all'>Return to homepage</Link>
+      <Image src={ErrorImage} alt="404 Not Found" />
+      <p className='font-bold text-3xl mb-3'>Page not found!</p>
+      <Link href={"/"} className='font-semibold underline underline-offset-2 hover:text-primary-500 transition-all'>Return to homepage</Link>
     </div>
   )
 }
-
-export default error
