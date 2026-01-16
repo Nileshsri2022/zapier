266ee359c772f2489860e4c4189ec272ef9f3708
Nileshsri2022 <2022021140@mmmut.ac.in>
2026-01-16 22:58:04 +0530

    feat: Switch Solana to devnet for testing

--- apps/worker/src/sendSolana.ts
+++ apps/worker/src/sendSolana.ts
@@ -1,9 +1,9 @@
 import { Keypair, LAMPORTS_PER_SOL, SystemProgram, Transaction, PublicKey, sendAndConfirmTransaction, Connection } from "@solana/web3.js";
 const base58 = require("bs58");
 
-const connection = new Connection("https://api.mainnet-beta.solana.com", "finalized");
+const connection = new Connection("https://api.devnet.solana.com", "finalized");
 
-export async function sendSol(address: string, amount: string)  {
+export async function sendSol(address: string, amount: string) {
     const keypair = Keypair.fromSecretKey(base58.decode((process.env.SOL_PRIVATE_KEY ?? "")));
     console.log(keypair.publicKey);
 
